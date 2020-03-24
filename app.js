fc0c318b99f1e293d34e474bb08331dc477610e3
afratetlay <afratetlay@gmail.com>
2020-03-24 08:01:42 +0000

    working on my weather forecast

---
+++ app.js
@@ -0,0 +1,7 @@
+new hoverEffect({
+    parent: docuemnt.querySelector('distortion'),
+    intensity: 0.2,
+    image1: './weather-app.jpg',
+    image2: './forecast.jpg',
+    displacementImage: './cloud.png'
+});
