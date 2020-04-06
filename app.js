3c440f8c0832bff43abf68f817ae104431f1896c
afratetlay <afratetlay@gmail.com>
2020-04-06 10:58:48 +0000

    working on my modal

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var toggleStatus = 1;
         
 /* End of the Toggle NavBar Menu */
 
-/* Beginning of the To Do List */
+/* Beginning of the To Do List Header */
 
 const text = baffle(".fancy");
 text.set({
@@ -23,7 +23,37 @@ text.set({
 text.start();
 text.reveal(4000);
 
-/* End of the To Do List */ 
+/* End of the To Do List Header */ 
+
+/*modal*/
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+/*modal*/
+
 
 
 
